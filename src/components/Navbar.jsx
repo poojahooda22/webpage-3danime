2919097169094e076d8fbac45d7c96dd24633206
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-04-26 17:15:45 +0530

    updated navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,7 +5,7 @@ function Navbar() {
     <div>
       <div className="navbar fixed w-full px-[4vw] py-[5vw] sm:px-[2vw] sm:py-[1.5vw] flex items-center justify-between cursor-pointer ">
         <div className="title font-semibold sm:text-[1.5vw]">Tarragon</div>
-        <div className=" hidden middle sm:flex gap-10 text-[1.3vw]">
+        <div className=" hidden middle sm:flex gap-10 text-[1.3vw] text-[#646464]">
           <h4>Products</h4>
           <h4>About</h4>
           <h4>Insights</h4>
