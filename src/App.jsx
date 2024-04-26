ae63f18507d70727f233dc83ef3f1fb73376d01e
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-04-26 17:40:50 +0530

    added

--- src/App.jsx
+++ src/App.jsx
@@ -63,7 +63,8 @@ function App() {
       <div className='btn absolute bottom-[16vw] left-[36vw] sm:bottom-[2.3vw] sm:left-1/2 sm:-translate-x-1/2 cursor-pointer'>
         <button 
           className='sm:text-[1.2vw] text-[4vw] px-[3vw] py-[2vw] sm:px-[1.3vw] sm:py-[.6vw]  
-          bg-white text-[black] rounded-full whitespace-nowrap '>Click to Slide</button>
+          bg-white text-[black] rounded-full 
+          whitespace-nowrap '>Click to Slide</button>
       </div>
     </main>
   )
