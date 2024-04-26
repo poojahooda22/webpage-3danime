70d8beefe2c5dec6ad034b43239231d6c30ce042
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-04-26 17:30:22 +0530

    added

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -14,7 +14,7 @@ function Navbar() {
           <img className="w-[8vw] sm:w-[1.8vw]" src="/images/icons/search.svg"/>     
         </div>
       </div>
-      {/* <div className='btn absolute 
+      {/* <div className='btn abs
         
       </div> */}
 
